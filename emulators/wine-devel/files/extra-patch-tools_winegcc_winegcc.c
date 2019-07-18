39d57ba7c0d49be117e5748c129e3714b7f7b6af
Alex S <shkhln@users.noreply.github.com>
2019-07-19 01:36:19 +0300

    Fix wine build.

--- emulators/wine-devel/files/extra-patch-tools_winegcc_winegcc.c
+++ emulators/wine-devel/files/extra-patch-tools_winegcc_winegcc.c
@@ -1,9 +1,9 @@
---- tools/winegcc/winegcc.c.orig       2018-12-28 18:58:40 UTC
-+++ tools/winegcc/winegcc.c
-@@ -396,6 +396,7 @@ static const strarray* get_lddllflags( const struct op
-     case PLATFORM_ANDROID:
-     case PLATFORM_SOLARIS:
-     case PLATFORM_UNSPECIFIED:
-+        strarray_add( flags, strmake( "-fuse-ld=%s", build_tool_name( opts, "ld", LD )));
-         strarray_add( flags, "-shared" );
-         strarray_add( flags, "-Wl,-Bsymbolic" );
+--- tools/winegcc/winegcc.c.orig	2019-05-10 18:43:42.000000000 +0300
++++ tools/winegcc/winegcc.c	2019-05-12 01:14:51.319923000 +0300
+@@ -472,6 +472,7 @@
+ 
+     /* generic Unix shared library flags */
+ 
++    strarray_add( flags, strmake( "-fuse-ld=%s", build_tool_name( opts, "ld", LD )));
+     strarray_add( flags, "-shared" );
+     strarray_add( flags, "-Wl,-Bsymbolic" );
