c85ddb5fdb1360b1272ca85171feb6495f81c023
Alex S <shkhln@users.noreply.github.com>
2019-07-19 01:36:28 +0300

    Build lib32-wine-devel with (multilib) GCC.

--- emulators/wine-devel/files/extra-patch-tools_winegcc_winegcc.c
+++
@@ -1,9 +0,0 @@
---- tools/winegcc/winegcc.c.orig	2019-05-10 18:43:42.000000000 +0300
-+++ tools/winegcc/winegcc.c	2019-05-12 01:14:51.319923000 +0300
-@@ -472,6 +472,7 @@
- 
-     /* generic Unix shared library flags */
- 
-+    strarray_add( flags, strmake( "-fuse-ld=%s", build_tool_name( opts, "ld", LD )));
-     strarray_add( flags, "-shared" );
-     strarray_add( flags, "-Wl,-Bsymbolic" );
